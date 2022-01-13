c642efe4c4cd250a12db1ddcf39db967754a976f
jhonpulido1990 <3825@holbertonschool.com>
2022-01-13 12:17:54 -0500

    function sub

--- funtion2.c
+++ funtion2.c
@@ -28,3 +28,23 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+/**
+ * sub - subtracts the top element of the stack
+ *           from the second top element of the stack
+ * @stack: pointer to the given node
+ * @ln: line nunber
+ * Return: returns nothing
+ */
+void sub(stack_t **stack, unsigned int line_numb)
+{
+	stack_t *tmp;
+
+	tmp = *stack;
+	if (tmp == NULL || tmp->next == NULL)
+	{
+		printf("L%u: can't sub, stack too short\n", line_numb);
+		exit(EXIT_FAILURE);
+	}
+	tmp->next->n -= tmp->n;
+	pop(stack, line_numb);
+}
